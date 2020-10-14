9e89f5fc6f7aeb83be585cd85df8a4b2860f55f8
anaismartell <martell.anais@gmail.com>
2020-10-14 16:23:57 +0200

    first commit

---
+++ frontend/src/components/Accueil.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-app id="homePage">
+        <top-header/>
+            <div class="logo">
+                <v-img src="../assets/icon-left-font-monochrome-black.png" id ="logo"></v-img>
+            </div>  
+            <h1 class="ml-12">Bienvenue sur l'Intranet de Groupomania !</h1>
+            <router-view></router-view>
+            <v-carousel
+                id="carousel" cycle height="auto" hide-delimiters :show-arrows="false">
+                <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
+            </v-carousel>      
+    </v-app>
+</template>
+<script>
+import Header from "./Header"
+
+export default {
+    name: "Accueil",
+    data(){
+        return{
+            items: [
+            {
+                src: require("../assets/images/halloween.jpg"),
+            },
+            {
+                src: require("../assets/images/teletravail.png"),
+            },
+            {
+                src: require("../assets/images/vacances.jpg"),
+            },
+            ]
+            
+        }
+    },
+    components: {
+        "top-header": Header,
+    },
+}
+</script>
+<style lang="scss">
+    #homePage{
+        background-color: #fcdcdc;
+    }
+    #carousel{
+        width: 60%;
+        margin-top: 5%;
+        margin-bottom: 5%;
+        margin-left: 20%;
+    }
+    .header{
+        background-color: rgb(253,45,1);
+        color: white;
+    }
+    @media screen and (min-width: 200px) and (max-width: 640px) {
+    #carousel {
+    padding-right: 15%;
+    width: 80%;
+    } 
+}
+        
+    
+</style>
---
+++ frontend/src/components/Auth.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-app id="auth">
+        
+        <v-container class="auth">
+            <div class="logo">
+                <v-img src="../assets/icon-left-font-monochrome-black.png" id ="logo"></v-img>
+            </div>  
+            <div class="auth__boutons">
+                <v-btn class="auth__boutons--ind" color="black white--text" @click="component='signup'">S'inscrire</v-btn>
+                <v-btn class="auth__boutons--ind" color="black white--text white--hover" @click="component='login'">Se connecter</v-btn>
+            </div>
+            
+            <component v-bind:is="component"></component>
+            
+        </v-container>
+        
+    </v-app>
+</template>
+
+<script>
+    import Signup from "./Signup.vue"
+    import Login from "./Login.vue"
+
+    export default {
+        name : "Auth",
+        data(){
+            return{
+                component: ""
+            }
+        },
+        
+        components: {
+            'login': Login,
+            'signup': Signup
+        }
+    }
+</script>
+
+<style lang="scss">
+    #auth {
+        display: flex;
+        flex-direction: row;
+    }
+    .auth {
+        .logo {
+            align-items: center;
+            margin-left:40%;
+        }
+            &__boutons{
+                display: flex;
+                flex-direction: row;
+                justify-content: space-around;
+                align-items: center;
+                margin-bottom: 2%;
+                &--ind{
+                    margin-top: 5%;
+                    width: 150px ;
+                    
+                }
+        }
+    }
+    
+</style>
